169b490059bd8430e002a5add247ec285e06f6a0
ChuWarm <ion7678@gmail.com>
2025-02-05 16:25:31 +0900

    씬 전환 구현
    Fade In/ Fade Out 효과 적용

---
+++ Assets/Resources/SceneLoadHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadHandler : MonoBehaviour
+{
+    public CanvasGroup fadePanel;
+    void Awake()
+    {
+        //씬이 변경될때 실행
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode) // 페이드 인,아웃 효과
+    {
+        fadePanel.alpha = 1;
+        fadePanel.DOFade(0, 3f).SetEase(Ease.InOutQuad); // 처음엔 느리고 점점빨라짐
+    }
+    void OnDestroy()
+    {
+        // 씬이 변경될 때 메모리 누수를 방지하기 위해 이벤트 해제
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}
