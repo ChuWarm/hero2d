01815bfabe97bd86c447467212f8be396a71abfd
jojunhyung <cka830611@naver.com>
2025-02-05 20:08:45 +0900

    SubUI
    Esc 눌렀을때 서브 메뉴  게임 종료 , 계속하기 기능 추가
    다시하기는 추후 추가해야될듯
    SubUI 스크립트 내용을 게임매니저에 넣어도 될듯

--- Assets/Scripts/SubUI.cs
+++ Assets/Scripts/SubUI.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class SubUI : MonoBehaviour
 {
+    public GameObject subUI;
     
+    public void Continuing()
+    {
+        // 계속하기 기능
+        subUI.SetActive(false);
+    }
+
+    private void ReStart()
+    {
+        // 캐릭터 능력치 장비 등등 저장하고 현재 씬 다시 실행 기능들은 게임 매니저에 구현해도 될듯
+    }
+    public void Toexit()
+    {
+        // 게임 종료
+        Application.Quit();
+    }
+    public void Esc()
+    {
+        // ESC키 눌렀을때 메뉴 창 띄우기 지우기
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(subUI.activeSelf) 
+                subUI.SetActive(false);
+            else
+                subUI.SetActive(true);
+        }
+    }
+    void Update()
+    {
+        Esc();
+    }
 }
